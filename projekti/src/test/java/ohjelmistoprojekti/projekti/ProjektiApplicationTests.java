d035ff8b4a077a9b89c864424397f3d6ac989fe2
annelitorvi <anneli.torvi@myy.haaga-helia.fi>
2026-09-01 14:05:19 +0300

    first commit

---
+++ projekti/src/test/java/ohjelmistoprojekti/projekti/ProjektiApplicationTests.java
@@ -0,0 +1,13 @@
+package ohjelmistoprojekti.projekti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
