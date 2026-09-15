647a8222bc86dd819e50de729b768da2b34f6a8e
annelito63 <anneli.torvi@myy.haaga-helia.fi>
2026-09-15 19:01:26 +0300

    controller

---
+++ projekti/src/main/java/ohjelmistoprojekti/projekti/web/TapahtumaController.java
@@ -0,0 +1,8 @@
+package ohjelmistoprojekti.projekti.web;
+
+import ohjelmistoprojekti.projekti.model.Tapahtuma;
+import ohjelmistoprojekti.projekti.model.TapahtumaRepository;
+
+public class TapahtumaController {
+    
+}
--- projekti/src/main/java/ohjelmistoprojekti/web/TapahtumaController.java
+++
@@ -1,5 +0,0 @@
-package ohjelmistoprojekti.web;
-
-public {
-    
-}
