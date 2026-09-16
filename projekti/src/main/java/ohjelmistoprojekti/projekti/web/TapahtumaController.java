676d71352b4d8f37de3c43ca897dfaa42efbb151
bhq723@myy.haaga-helia.fi <bhq723@myy.haaga-helia.fi>
2026-09-16 19:05:59 +0300

    POST tehty

--- projekti/src/main/java/ohjelmistoprojekti/projekti/web/TapahtumaController.java
+++ projekti/src/main/java/ohjelmistoprojekti/projekti/web/TapahtumaController.java
@@ -1,10 +1,13 @@
 package ohjelmistoprojekti.projekti.web;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,9 +23,16 @@ public class TapahtumaController {
         this.tapahtumaRepository = tapahtumaRepository;
     }
 
-     @GetMapping("/tapahtumat")
-    public Iterable<Tapahtuma> findAllTapahtumat(){
+    @GetMapping("/tapahtumat")
+    public Iterable<Tapahtuma> findAllTapahtumat() {
         return tapahtumaRepository.findAll();
+    }
+
+    @PostMapping("/tapahtumat")
+    public ResponseEntity<Tapahtuma> addTapahtuma(@RequestBody Tapahtuma uusiTapahtuma) {
+        uusiTapahtuma.setTapahtumaid(null); // kanta generoi id:n itse
+        Tapahtuma tallennettu = tapahtumaRepository.save(uusiTapahtuma);
+        return ResponseEntity.status(HttpStatus.CREATED).body(tallennettu);
     }
 
 }
